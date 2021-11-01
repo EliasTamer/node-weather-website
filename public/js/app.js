be5105975ae72482d87737a4d93f34914dd00c94
unknown <eliastamer3@gmail.com>
2021-11-01 11:30:29 -0700

    Console log message

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,4 @@
+console.log('this is a test');
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
